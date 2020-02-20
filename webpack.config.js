cfdc684ff7f1fffaf281de990a3db116e30baac8
alex.harutyunyan <alex.harutyunyan@synisys.com>
2020-02-20 09:34:34 +0400

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        hot: true,
+        port: 9090,
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ]
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ],
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./public/index.html"
+        }),
+    ]
+};
